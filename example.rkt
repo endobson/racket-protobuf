209d9a81f20ec2abcbefa660126410f142945eb4
Eric Dobson <eric.n.dobson@gmail.com>
2015-01-20 21:05:27 -0800

    Make code generation use new message foramt for bootstraping.

--- example.rkt
+++ example.rkt
@@ -5,18 +5,112 @@
     racket/base
     racket/syntax
     racket/list
+    racket/pretty
+    racket/match
+    
+    (for-syntax
+      syntax/parse
+      syntax/parse/experimental/template
+      racket/base)
     "code-generation.rkt")
   "proto-reader.rkt"
+  racket/pretty
   racket/port
   )
 
 
 (define pb (call-with-input-file* (expand-user-path "~/tmp/tmp.pb") port->bytes))
+(define pb2 (call-with-input-file* (expand-user-path "~/tmp/descriptor.pb") port->bytes))
 
 (define proto-port (open-input-bytes pb))
+(define proto-port2 (open-input-bytes pb2))
+
+(begin-for-syntax
+  (begin-for-syntax
+    (define-syntax-class proto-type
+      (pattern (~datum string))
+      (pattern (~datum boolean))
+      (pattern (~datum int32))
+      (pattern type-ref:str))))
+
+
 
 
 (begin-for-syntax
+  (define-syntax message
+    (syntax-parser 
+      [(_ name:str
+          ((~and field-multiplicity (~or (~datum optional) (~datum repeated)))
+           field-type:proto-type
+           field-name:id
+           (~datum =)
+           field-number:exact-positive-integer
+           ) ...)
+        (template
+          (message-descriptor 
+            name
+            (hash
+              (?@ field-number
+                  (field-descriptor
+                    'field-multiplicity
+                    'field-type)) ...)))]))
+  (define mds2
+    (list
+      (message "FileDescriptorSet"
+        [repeated "FileDescriptorProto" file = 1])
+      (message "FileDescriptorProto"
+        [optional string name = 1]
+        [optional string package = 2]
+        [repeated string dependency = 3]
+        [repeated int32 public_dependency = 10]
+        [repeated int32 weak_dpendency = 11]
+        [repeated "DescriptorProto" message_type = 4]
+        [repeated "EnumDescriptorProto" enum_type = 5]
+        #;
+        [repeated "ServiceDescriptorProto" service = 6]
+        #;
+        [repeated "FieldDescriptorProto" extension = 7]
+        #;
+        [optional "FileOptions" options = 8]
+        #;
+        [optional "SourceCodeInfo" source_code_info = 9])
+      (message "DescriptorProto"
+        [optional string name = 1]
+        #;
+        [repeated "FieldDescriptorProto" field = 2]
+        #;
+        [repeated "FieldDescriptorProto" extension = 6]
+        [repeated "DescriptorProto" nested_type = 3]
+        #;
+        [repeated "EnumDescriptorProto" enum_type = 4]
+        #;
+        [repeated "ExtensionRange" extension_range = 5]
+        #;
+        [repeated "OneofDescriptorProto" oneof_decl = 8]
+        #;
+        [optional "MessageOptions" options = 7])
+      (message "EnumDescriptorProto"
+        [optional string name = 1]
+        #;
+        [repeated "EnumValueDescriptorProto" value = 2]
+        #;
+        [repeated "EnumOptions" options = 3])
+      (message "FieldDescriptorProto"
+        [optional string name = 1]
+        [optional int32 number = 3]
+        #;
+        [optional "Label" label = 4]
+        #;
+        [optional "Type" type = 5]
+        [optional string type_name = 6]
+        [optional int32 oneof_index = 9])
+
+      
+      ))
+  (define mds3
+    (for/hash ([md (in-list mds2)])
+      (values (message-descriptor-name md) md)))
+
   (define descriptors
     (hash
       "FileDescriptorSet"
@@ -102,7 +196,7 @@
       ))
 
 
-  (define mds
+  (define mds4
     (for/hash ([(name fields) (in-hash descriptors)])
       (values name
         (message-descriptor 
@@ -114,19 +208,21 @@
                 (first field)
                 (if (equal? 'message (second field))
                     (third field)
-                    (second field))))))))))
+                    (second field)))))))))
+  (define mds mds3))
 
 
 (define-syntax (go stx)
   (define mids
-    (for/hash ([(name fields) (in-hash descriptors)])
+    (for/hash ([(name md) (in-hash mds)])
       (values name
         (message-identifiers
           (format-id stx "~a" name)
-          (for/hash ([(field-number field) fields])
+          (for/hash ([(field-number fd) (message-descriptor-fields md)])
+            (match-define (field-descriptor multiplicity type) fd)
             (values
               field-number
-              ((if (equal? 'optional (first field))
+              ((if (equal? 'optional multiplicity)
                    singular-field-identifiers
                    repeated-field-identifiers)
                 (format-id stx "~a-~a" name field-number)
@@ -142,7 +238,13 @@
 
 (go)
 
-(FileDescriptorSet-parser proto-port (FileDescriptorSet))
+(pretty-print
+  (FileDescriptorSet-parser proto-port (FileDescriptorSet)))
+(pretty-print
+  (FileDescriptorSet-parser proto-port2 (FileDescriptorSet)))
+
+
+
 
 ;(for ([(name fields) (in-hash descriptors)])
 ;  (set-message-descriptor-fields!
