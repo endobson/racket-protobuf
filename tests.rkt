957e5793aad605d3973a73c2787a18b4eccce8ef
Eric Dobson <eric.n.dobson@gmail.com>
2015-06-06 19:37:45 -0700

    Fill in more parts of descriptor proto definition.

--- tests.rkt
+++ tests.rkt
@@ -33,25 +33,32 @@
     [optional "SourceCodeInfo" source_code_info = 9])
   (message "DescriptorProto"
     [optional string name = 1]
-    #;
     [repeated "FieldDescriptorProto" field = 2]
     #;
     [repeated "FieldDescriptorProto" extension = 6]
     [repeated "DescriptorProto" nested_type = 3]
-    #;
     [repeated "EnumDescriptorProto" enum_type = 4]
     #;
     [repeated "ExtensionRange" extension_range = 5]
     #;
     [repeated "OneofDescriptorProto" oneof_decl = 8]
     #;
-    [optional "MessageOptions" options = 7])
+    [optional "MessageOptions" options = 7]
+    [repeated "ReservedRange" reserved_range = 9]
+    [optional string reserved_name = 10])
+  (message "ReservedRange"
+    [optional int32 start = 1]
+    [optional int32 end = 2])
   (message "EnumDescriptorProto"
     [optional string name = 1]
-    #;
     [repeated "EnumValueDescriptorProto" value = 2]
     #;
     [repeated "EnumOptions" options = 3])
+  (message "EnumValueDescriptorProto"
+    [optional string name = 1]
+    [optional int32 number = 2]
+    #;
+    [optional "EnumValueOptions" options = 3])
   (message "FieldDescriptorProto"
     [optional string name = 1]
     [optional int32 number = 3]
