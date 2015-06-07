87d1d7da3712cc9b1529b81a4a7a419b064ee8c5
Eric Dobson <eric.n.dobson@gmail.com>
2015-06-07 00:31:54 -0700

    Cleanup of parser code.

--- code-generation/parser.rkt
+++ code-generation/parser.rkt
@@ -21,10 +21,70 @@
     [(or 'int32 'boolean)
      'varint]))
 
+
 (define (generate-parser proto-ids desc)
   (define name  (message-descriptor-name desc))
   (define message-ids (proto-identifiers-message (hash-ref proto-ids name)))
   (define builder-ids (proto-identifiers-builder (hash-ref proto-ids name)))
+
+  (define (optional-field-clause field-number type)
+    (match-define (builder-singular-field-identifiers accessor* mutator* _ _)
+      (hash-ref (builder-identifiers-fields builder-ids) field-number))
+    (define/with-syntax accessor accessor*)
+    (define/with-syntax mutator mutator*)
+    (case type
+     [(string)
+      #`(let ([amount (read-proto-varint port)])
+          (mutator current-builder (read-proto-string port amount)))]
+     [(bytes) #`(mutator current-builder
+                 (read-proto-bytes port (read-proto-varint port)))]
+     ;; TODO make this cap in 32 bit range
+     [(int32) #`(mutator current-builder (read-proto-varint port))]
+     ;; TODO make this work
+     [(boolean) #`(error 'nyi)]
+     [else
+       (define sub-ids (proto-identifiers-builder (hash-ref proto-ids type)))
+       (define/with-syntax sub-constructor
+         (builder-identifiers-constructor sub-ids))
+       (define/with-syntax sub-parser
+         (builder-identifiers-parser sub-ids))
+       #'(let ()
+           (define sub-builder
+             (let ([old-value (accessor current-builder)])
+               (or old-value
+                   (let ([new-value (sub-constructor)])
+                     (mutator current-builder new-value)
+                     new-value))))
+           (define len (read-proto-varint port))
+           (sub-parser
+             (make-limited-input-port port len)
+             sub-builder))]))
+
+  (define (repeated-field-clause field-number type)
+    (match-define (builder-repeated-field-identifiers _ accessor* _ adder* _ _ _ _ _ _)
+      (hash-ref (builder-identifiers-fields builder-ids) field-number))
+    (define/with-syntax accessor accessor*)
+    (define/with-syntax adder adder*)
+    #`(adder current-builder
+        #,(case type
+           [(string) #'(read-proto-string port (read-proto-varint port))]
+           [(bytes) #'(read-proto-bytes port (read-proto-varint port))]
+           ;; TODO make this cap in 32 bit range
+           [(int32) #'(read-proto-varint port)]
+           ;; TODO make this work
+           [(boolean) #`(error 'nyi)]
+           [else
+             (define sub-ids (hash-ref proto-ids type))
+             (define/with-syntax sub-constructor
+               (builder-identifiers-constructor (proto-identifiers-builder sub-ids)))
+             (define/with-syntax sub-parser
+               (builder-identifiers-parser (proto-identifiers-builder sub-ids)))
+             #'(let ([len (read-proto-varint port)])
+                 (sub-parser
+                   (make-limited-input-port port len)
+                   (sub-constructor)))])))
+
+
   (define field-clauses
     (for/list ([(field-number fd) (message-descriptor-fields desc)])
       (match-define (field-descriptor multiplicity type _) fd)
@@ -33,67 +93,15 @@
            (error 'parse-proto "Bad wire type"))
          #,(case multiplicity
              [(optional)
-              (match-define (builder-singular-field-identifiers accessor* mutator* _ _)
-                (hash-ref (builder-identifiers-fields builder-ids) field-number))
-              (define/with-syntax accessor accessor*)
-              (define/with-syntax mutator mutator*)
-              (case type
-               [(string)
-                #`(let ([amount (read-proto-varint port)])
-                    (mutator current-builder (read-proto-string port amount)))]
-               [(bytes) #`(mutator current-builder
-                           (read-proto-bytes port (read-proto-varint port)))]
-               ;; TODO make this cap in 32 bit range
-               [(int32) #`(mutator current-builder (read-proto-varint port))]
-               ;; TODO make this work
-               [(boolean) #`(error 'nyi)]
-               [else
-                 (define sub-ids (proto-identifiers-builder (hash-ref proto-ids type)))
-                 (define/with-syntax sub-constructor
-                   (builder-identifiers-constructor sub-ids))
-                 (define/with-syntax sub-parser
-                   (builder-identifiers-parser sub-ids))
-                 #'(let ()
-                     (define sub-builder
-                       (let ([old-value (accessor current-builder)])
-                         (or old-value
-                             (let ([new-value (sub-constructor)])
-                               (mutator current-builder new-value)
-                               new-value))))
-                     (define len (read-proto-varint port))
-                     (sub-parser
-                       (make-limited-input-port port len)
-                       sub-builder))])]
+              (optional-field-clause field-number type)]
              [(repeated)
-              (match-define (builder-repeated-field-identifiers _ accessor* _ adder* _ _ _ _ _ _)
-                (hash-ref (builder-identifiers-fields builder-ids) field-number))
-              (define/with-syntax accessor accessor*)
-              (define/with-syntax adder adder*)
-              #`(adder current-builder
-                  #,(case type
-                     [(string) #'(read-proto-string port (read-proto-varint port))]
-                     [(bytes) #'(read-proto-bytes port (read-proto-varint port))]
-                     ;; TODO make this cap in 32 bit range
-                     [(int32) #'(read-proto-varint port)]
-                     ;; TODO make this work
-                     [(boolean) #`(error 'nyi)]
-                     [else
-                       (define sub-ids (hash-ref proto-ids type))
-                       (define/with-syntax sub-constructor
-                         (builder-identifiers-constructor (proto-identifiers-builder sub-ids)))
-                       (define/with-syntax sub-parser
-                         (builder-identifiers-parser (proto-identifiers-builder sub-ids)))
-                       #'(let ([len (read-proto-varint port)])
-                           (sub-parser
-                             (make-limited-input-port port len)
-                             (sub-constructor)))]))])]))
+              (repeated-field-clause field-number type)])]))
 
-
-  (define message-parser (message-identifiers-parser message-ids))
-  (define builder-parser (builder-identifiers-parser builder-ids))
+  (define/with-syntax message-parser (message-identifiers-parser message-ids))
+  (define/with-syntax builder-parser (builder-identifiers-parser builder-ids))
 
   #`(begin
-      (define (#,builder-parser port current-builder)
+      (define (builder-parser port current-builder)
           (let loop ()
             (define varint (read-proto-varint port #t))
             (unless (eof-object? varint)
@@ -112,6 +120,6 @@
           current-builder)
 
 
-      (define (#,message-parser port)
+      (define (message-parser port)
         (#,(message-identifiers-freezer message-ids)
-         (#,builder-parser port (#,(builder-identifiers-constructor builder-ids)))))))
+         (builder-parser port (#,(builder-identifiers-constructor builder-ids)))))))
